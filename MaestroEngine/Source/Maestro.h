801deb3cfafcf537ab4edb91dd4212775cfac4da
Dan Yeomans <danyeomans98@gmail.com>
2018-02-08 01:53:14 -0500

    Update documentation

--- MaestroEngine/Source/Maestro.h
+++ MaestroEngine/Source/Maestro.h
@@ -23,15 +23,45 @@ class Maestro : public Updatable
 {
 // methods
 public:
-	
+		
+	/// <summary>
+	/// Default constructor.
+	/// </summary>
 	Maestro() {};
+	
+	/// <summary>
+	/// Virtual destructor.
+	/// </summary>
 	virtual ~Maestro() override {};
-
+	
+	/// <summary>
+	/// Called once when the Updatable is created, before OnStart or OnUpdate.
+	/// </summary>
 	virtual void OnCreate() override;
+	
+	/// <summary>
+	/// Called once during the update loop for this Updatable, before the first OnUpdate.
+	/// </summary>
 	virtual void OnStart() override;
+	
+	/// <summary>
+	/// Called during the update loop for this Updatable, and is called at least once per frame.
+	/// </summary>
 	virtual void OnUpdate() override;
+
+	/// <summary>
+	/// Called following OnUpdate for this Updatable, and is called as many times as possible per frame (or once per frame if vsync is enabled).
+	/// </summary>
 	virtual void OnRender() override;
+	
+	/// <summary>
+	/// Called once after the update loop for this Updatable, before any Updatables in this object's update loop are destroyed.
+	/// </summary>
 	virtual void OnFinish() override;
+	
+	/// <summary>
+	/// Called after OnFinish for this Updatable, before the object is destroyed.
+	/// </summary>
 	virtual void OnDestroy() override;
 
 	/// <summary>
--- MaestroEngine/Source/System.h
+++ MaestroEngine/Source/System.h
@@ -13,10 +13,24 @@ class System : public Updatable
 {
 // methods
 public:
-
+	
+	/// <summary>
+	/// Initializes a new instance of the <see cref="System"/> class.
+	/// </summary>
+	/// <param name="engine">The <see cref="Maestro"/> engine instance.</param>
 	System(Maestro *const engine);
+	
+	/// <summary>
+	/// Virtual destructor.
+	/// </summary>
 	virtual ~System() override {};
-
+	
+	/// <summary>
+	/// Called when a new <see cref="Component"/> that has been claimed by this <see cref="System"/> (using <see cref="Maestro::SetManagingSystem" />) is requested by a Component or <see cref="Entity"/>.
+	/// </summary>
+	/// <param name="srcEnt">The source Entity.</param>
+	/// <param name="cmpType">The Component's type_index.</param>
+	/// <returns>A const pointer to the created Component, stored internally by the System.</returns>
 	virtual Component*const OnComponentCreate(Entity *const srcEnt, std::type_index cmpType);
 	virtual bool OnComponentDestroy(Entity *const srcEnt, Component *const srcCmp);
 
