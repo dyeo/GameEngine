9c9740c895a2f4248d24dd012c01828fc3cb308d
Dan Yeomans <danyeomans98@gmail.com>
2018-02-08 22:55:18 -0500

    New Maestro class for interfacing with the user/developer

---
+++ MaestroEngine/Source/Maestro.h
@@ -0,0 +1,19 @@
+#ifndef _MAESTRO_MAESTRO_H_
+#define _MAESTRO_MAESTRO_H_
+
+namespace mae
+{	
+	/// <summary>
+	/// Static class responsible for safe and simple user-interface with the engine.
+	/// </summary>
+	class Maestro
+	{
+		// methods
+	public:
+
+		// members
+	private:
+	};
+}
+
+#endif // !_MAESTRO_MAESTRO_H_
